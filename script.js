d889dfd3c15989a9f48e33e2f0edeb54dbaa0e8b
Andrei12311 <xboxgamepass908@gmail.com>
2025-04-27 22:15:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,19 +15,18 @@ function updateCountdown() {
     const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-    countdownElements.days.textContent = days;
-    countdownElements.hours.textContent = hours;
-    countdownElements.minutes.textContent = minutes;
-    countdownElements.seconds.textContent = seconds;
+    countdownElements.days.textContent = days + ' zile';
+    countdownElements.hours.textContent = hours + ' ore';
+    countdownElements.minutes.textContent = minutes + ' minute';
+    countdownElements.seconds.textContent = seconds + ' secunde';
 
     if (timeRemaining <= 0) {
         clearInterval(interval);
-        countdownElements.days.textContent = "0";
-        countdownElements.hours.textContent = "0";
-        countdownElements.minutes.textContent = "0";
-        countdownElements.seconds.textContent = "0";
+        countdownElements.days.textContent = "0 zile";
+        countdownElements.hours.textContent = "0 ore";
+        countdownElements.minutes.textContent = "0 minute";
+        countdownElements.seconds.textContent = "0 secunde";
     }
 }
 
-// Updatează numărătoarea la fiecare secundă
 const interval = setInterval(updateCountdown, 1000);
